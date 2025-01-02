c35429d0b49ad18d15f3ec99941d92fa414c613f
paulLieber98 <paullieber07@gmail.com>
2025-01-01 18:00:51 -0800

    Add styled navbar and about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,40 @@
 import React from 'react';
-import { Box, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
+
+// Extend the theme to include custom colors and styles
+const theme = extendTheme({
+  colors: {
+    cream: '#FFF8F0',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'cream',
+      },
+    },
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: 'medium',
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router>
-        <Box
-          minH="100vh"
-          pt="60px"
-          position="relative"
-          bgGradient="linear(to-br, blue.50, purple.50, blue.50)"
-          _after={{
-            content: '""',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backdropFilter: 'blur(100px)',
-            zIndex: -1,
-          }}
-        >
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Box>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </Router>
     </ChakraProvider>
   );
